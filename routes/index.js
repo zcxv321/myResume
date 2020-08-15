7fe58691eb3feb87a2edee44666b409887292f0f
wannit <wannit.b@ku.th>
2020-08-16 00:54:38 +0700

    add scrollspy

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/home', function(req, res, next) {
+  res.render('home', { title: 'Express' });
+});
 
 module.exports = router;
